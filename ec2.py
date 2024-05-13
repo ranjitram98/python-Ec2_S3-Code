18a84a28a20a225192ba109181a8e168bd8ff1ca
ranjitram98 <147502548+ranjitram98@users.noreply.github.com>
2024-05-13 10:15:51 +0530

    Create ec2.py

---
+++ ec2.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+
+
+ec2 = boto3.client('ec2')
+
+def lambda_handler(event, context):
+    print(event)
+    
+    user = event['detail']['userIdentity']['type']
+
+    instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']
+
+
+    ec2.create_tags(
+        Resources=[
+           instanceId
+        ],
+        Tags=[
+            {
+            'Key': 'Owner',
+            'Value': user  # Using the 'user' variable instead of string 'user'
+            },
+        ]
+    )
+   
+    return
